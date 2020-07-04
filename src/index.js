a5ed40a60e1a24a5aa06b9c9a80c4129ea146aa8
unknown <you@example.com>
2020-07-04 20:36:01 +0300

    html structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,1 @@
-import { aaa } from './module';
 import './scss/index.scss';
-
-console.log('indx', aaa);
--- src/module.js
+++
@@ -1,7 +0,0 @@
-export const aaa = 'aaa';
-
-async function start() {
-  return await Promise.resolve('async is working');
-}
-
-start().then(console.log);
